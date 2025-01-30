d8bdc6f1084869fc0f9b54e47e1db639a7f3fd6e
Rjamez <robin.adhola.student@moringaschool.com>
2025-01-30 18:05:22 +0300

    done

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     is_verified = db.Column(db.Boolean, default=False)
     is_admin = db.Column(db.Boolean, default=False)
 
